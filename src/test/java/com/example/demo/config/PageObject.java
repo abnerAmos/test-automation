99cab7cbcc587bacfe672191d8edeb4daa1706cf
abnerAmos <abner.fsouza1@gmail.com>
2023-08-24 10:01:53 -0300

    Configuração PageObject

---
+++ src/test/java/com/example/demo/config/PageObject.java
@@ -0,0 +1,40 @@
+package com.example.demo.config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public abstract class PageObject {
+
+    protected WebDriver browser;
+
+    public PageObject(WebDriver browser) {
+
+        // ChromeOptions, serve para adicionar argumentos ao abrir o browser, inclusive o modo Headless
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--proxy-server='direct://'");
+        options.addArguments("--proxy-bypass-list=*");
+        options.addArguments("--start-maximized");
+        options.addArguments("--headless=new");
+
+        // Driver para conexão com o navegador
+        WebDriverManager.chromedriver().setup();
+
+        // Instancia do Driver do Chrome
+        if (browser != null)
+            this.browser = browser;
+        else
+            this.browser = new ChromeDriver(options);
+    }
+
+    public void closeBrowser() {
+        this.browser.quit();
+    }
+
+    public abstract PageObject goToThisPage();
+}
